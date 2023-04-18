f291052818eb94252e762ef1a23c7d20317bcaf2
unknown <anaemily.castilloperez.305@my.csun.edu>
2023-04-18 15:39:11 -0700

    Documentation comments finished

--- AirlineReservation/src/GUI/LoginFrame.java
+++ AirlineReservation/src/GUI/LoginFrame.java
@@ -14,7 +14,7 @@ import java.awt.event.ActionListener;
  * for a flight reservation system. This specific GUI called "LoginFrame" helps users to log into their alreadyd existing account.
  * <p>
  * <b>Explanation of important functions:</b> GUI implements user input into text fields by gathering username and password that
- * user wants has used to create the account. This information is saved in the system for supporting returning users.
+ * user has used to create the account. This information is saved in the system for supporting returning users.
  * <p>
  * <b>Important data structure in class/important methods in class:</b>
  * <ul>
@@ -35,7 +35,7 @@ public class LoginFrame extends JFrame {
 
     /**
      *
-     * Method for...
+     * Method is for creating and displaying a desktop window to a specific size as LoginFrame runs.
      *
      */
     public LoginFrame() {
@@ -44,18 +44,36 @@ public class LoginFrame extends JFrame {
         setSize(450,300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         logInButton.addActionListener(new ActionListener() {
+            /**
+             *
+             * Action listener used to code the Login button of current frame if user decides to log into their
+             * already existing account
+             *
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 loginUser();
             }
         });
         forgotPasswordButton.addActionListener(new ActionListener() {
+            /**
+             *
+             * Action listener used to code the forgot password button of current frame if user requires
+             * assistance in case of a forgotten password
+             *
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
         registerButton.addActionListener(new ActionListener() {
+            /**
+             *
+             * Action listener used to code the Register button of current frame if user decides to continue
+             * enrollment in the system.
+             *
+             */
             @Override
             public void actionPerformed(ActionEvent e) {
                 RegisterFrame registerFrame = new RegisterFrame();
@@ -71,7 +89,7 @@ public class LoginFrame extends JFrame {
      *
      * Method for error checking user input in text fields of the current frame.
      * Also checks if text fields are empty.
-     * Registers a new user by setting a new username and password or checks if account already exists.
+     * Logins a user by requiring username and password and checks if account already exists.
      *
      */
     private void loginUser() {
